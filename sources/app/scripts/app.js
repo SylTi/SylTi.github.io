683904075cf2fe8c14d42365ae886727497b836d
SylTi <syltilt@gmail.com>
2014-02-28 02:54:44 +0100

    changing pres stuff and adding google map geoloc

--- sources/app/scripts/app.js
+++ sources/app/scripts/app.js
@@ -5,7 +5,8 @@ angular.module('sylTigithubioApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'google-maps'
 ])
   .config(function ($routeProvider) {
     $routeProvider
--- sources/app/scripts/controllers/about.js
+++ sources/app/scripts/controllers/about.js
@@ -1,10 +1,21 @@
 'use strict';
 
 angular.module('sylTigithubioApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('AboutCtrl', function ($scope)
+	{
+		$scope.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+
+		$scope.map =
+		{
+			center:
+			{
+				latitude: 48.793521,
+				longitude: 2.398767
+			},
+			zoom: 12
+		};
+	});
